70d57a282f59e2238ee1104237d393a24cc32ac5
Cong Toan Doan <toandc1152@gmail.com>
2019-09-12 17:53:05 -0500

    Remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#mvn clean package -DskipTests=true
-
-docker build -t skygram-api .
-
-docker tag skygram-api toandc/skygram-api:0.0.1
-
-docker push toandc/skygram-api:0.0.1
