062a744c57d9ab84cfd29d7674191b2adda9c706
Paul A. Tumwine <paulatumwine@gmail.com>
2019-09-23 17:04:36 -0500

    Add test classes for our newly added functional pipelines

---
+++ src/test/java/com/skyteam/skygram/functional/PostFunctionalTest.java
@@ -0,0 +1,15 @@
+package com.skyteam.skygram.functional;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class PostFunctionalTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
